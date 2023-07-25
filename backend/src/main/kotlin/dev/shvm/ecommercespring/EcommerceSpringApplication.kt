1c497749080939599cdf4fedbd072fb26eaf2742
shivamsingha <shivamgit@pm.me>
2023-07-25 21:06:24 +0530

    Initial Commit

---
+++ backend/src/main/kotlin/dev/shvm/ecommercespring/EcommerceSpringApplication.kt
@@ -0,0 +1,11 @@
+package dev.shvm.ecommercespring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EcommerceSpringApplication
+
+fun main(args: Array<String>) {
+	runApplication<EcommerceSpringApplication>(*args)
+}
---
+++ backend/src/test/kotlin/dev/shvm/ecommercespring/EcommerceSpringApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.shvm.ecommercespring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EcommerceSpringApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
