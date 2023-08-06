5eb7acc30672015970ed196566480504e39b6c4c
shivamsingha <shivamgit@pm.me>
2023-08-06 19:13:51 +0530

    Update http request authorizations

--- backend/src/main/java/ecommercespring/backend/SecurityConfiguration.java
+++ backend/src/main/java/ecommercespring/backend/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package ecommercespring.backend;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -25,6 +26,15 @@ public class SecurityConfiguration {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers(
+                                HttpMethod.GET,
+                                "/",
+                                "/explorer/**",
+                                "/products/**",
+                                "/productCategories/**"
+                        )
+                        .permitAll()
+                        .requestMatchers("/discounts/**", "/orderItems/**").hasAnyRole("ADMIN", "USER", "SELLER")
                         .anyRequest().authenticated()
                 )
                 .oauth2Login(Customizer.withDefaults())
