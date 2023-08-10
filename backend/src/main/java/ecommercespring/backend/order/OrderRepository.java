6fe750b65e59c17c35d04cc290d9c6c4d87cde26
shivamsingha <shivamgit@pm.me>
2023-08-10 12:34:40 +0530

    Limit access to own orders only

--- backend/src/main/java/ecommercespring/backend/order/OrderRepository.java
+++ backend/src/main/java/ecommercespring/backend/order/OrderRepository.java
@@ -1,8 +1,14 @@
 package ecommercespring.backend.order;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.UUID;
 
 public interface OrderRepository extends JpaRepository<Order, UUID> {
+
+    @Query("select o from Order o where o.createdBy = ?#{ principal?.name }")
+    Page<Order> findAll(Pageable pageable);
 }
