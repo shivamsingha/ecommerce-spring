02c2ae1a2ac98779f08682280f055361b32c6077
shivamsingha <shivamgit@pm.me>
2023-08-10 12:34:10 +0530

    Configure security to include roles in GrantedAuthority

--- backend/src/main/java/ecommercespring/backend/SecurityConfiguration.java
+++ backend/src/main/java/ecommercespring/backend/SecurityConfiguration.java
@@ -1,18 +1,29 @@
 package ecommercespring.backend;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.data.repository.query.SecurityEvaluationContextExtension;
 import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInitiatedLogoutSuccessHandler;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
 
 @Configuration
 @EnableWebSecurity
@@ -20,6 +31,8 @@ import org.springframework.security.web.authentication.logout.LogoutSuccessHandl
 public class SecurityConfiguration {
 
     private final ClientRegistrationRepository clientRegistrationRepository;
+    @Value("${spring.security.oauth2.client.registration.keycloak.client-id}")
+    private String clientId;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -34,17 +47,42 @@ public class SecurityConfiguration {
                                 "/productCategories/**"
                         )
                         .permitAll()
-                        .requestMatchers("/discounts/**", "/orderItems/**").hasAnyRole("ADMIN", "USER", "SELLER")
                         .anyRequest().authenticated()
                 )
-                .oauth2Login(Customizer.withDefaults())
                 .oauth2ResourceServer(oauth2 -> oauth2
-                        .jwt(Customizer.withDefaults())
+                        .jwt(jwtConfigurer -> jwtConfigurer
+                                .jwtAuthenticationConverter(authenticationConverter(authoritiesConverter()))
+                        )
                 )
                 .logout(logout -> logout
                         .logoutSuccessHandler(oidcLogoutSuccessHandler())
                 );
         return http.build();
+    }
+
+    /**
+     * This is a converter for roles as embedded in the JWT by a Keycloak server
+     * Roles are taken from both realm_access.roles & resource_access.{client}.roles
+     */
+    @SuppressWarnings("unchecked")
+    @Bean
+    public Jwt2AuthoritiesConverter authoritiesConverter() {
+        return jwt -> {
+            final var realmAccess = (Map<String, Object>) jwt.getClaims().getOrDefault("realm_access", Map.of());
+            final var realmRoles = (Collection<String>) realmAccess.getOrDefault("roles", List.of());
+
+            final var resourceAccess = (Map<String, Object>) jwt.getClaims().getOrDefault("resource_access", Map.of());
+            final var confidentialClientAccess = (Map<String, Object>) resourceAccess.getOrDefault(clientId, Map.of());
+            final var confidentialClientRoles = (Collection<String>) confidentialClientAccess.getOrDefault("roles", List.of());
+
+            return Stream.concat(realmRoles.stream(), confidentialClientRoles.stream())
+                    .map(SimpleGrantedAuthority::new).toList();
+        };
+    }
+
+    @Bean
+    public Jwt2AuthenticationConverter authenticationConverter(Jwt2AuthoritiesConverter authoritiesConverter) {
+        return jwt -> new JwtAuthenticationToken(jwt, authoritiesConverter.convert(jwt));
     }
 
     @Bean
@@ -58,5 +96,11 @@ public class SecurityConfiguration {
         oidcLogoutSuccessHandler.setPostLogoutRedirectUri("{baseUrl}");
 
         return oidcLogoutSuccessHandler;
+    }
+
+    public interface Jwt2AuthoritiesConverter extends Converter<Jwt, Collection<? extends GrantedAuthority>> {
+    }
+
+    public interface Jwt2AuthenticationConverter extends Converter<Jwt, AbstractAuthenticationToken> {
     }
 }
