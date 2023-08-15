371c7d0460d467357ec9230ad1ba2f91bd6cee14
shivamsingha <shivamgit@pm.me>
2023-08-15 11:03:10 +0530

    Add basic navbar

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,7 +1,15 @@
 import type { Metadata } from "next";
 
+import {
+  MagnifyingGlassIcon,
+  ShoppingBagIcon,
+} from "@heroicons/react/24/outline";
+import { PropsWithChildren, FC } from "react";
 import { Inter } from "next/font/google";
 
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(" ");
+}
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -11,14 +19,86 @@ export const metadata: Metadata = {
   title: "Create Next App",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  );
-}
+const RootLayout: FC<PropsWithChildren> = ({ children }) => (
+  <html lang="en">
+    <body className={inter.className}>
+      <div className="bg-white">
+        <header className="relative bg-white">
+          <p className="flex h-10 items-center justify-center bg-indigo-600 px-4 text-sm font-medium text-white sm:px-6 lg:px-8">
+            Get free delivery on orders over $100
+          </p>
+
+          <nav
+            className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8"
+            aria-label="Top"
+          >
+            <div className="border-b border-gray-200">
+              <div className="flex h-16 items-center">
+                {/* Logo */}
+                <div className="ml-4 flex lg:ml-0">
+                  <a href="#">
+                    <span className="sr-only">Your Company</span>
+                    <img
+                      src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                      className="h-8 w-auto"
+                      alt=""
+                    />
+                  </a>
+                </div>
+
+                <div className="ml-auto flex items-center">
+                  <div className="hidden lg:flex lg:flex-1 lg:items-center lg:justify-end lg:space-x-6">
+                    <a
+                      className="text-sm font-medium text-gray-700 hover:text-gray-800"
+                      href="#"
+                    >
+                      Sign in
+                    </a>
+                    <span className="h-6 w-px bg-gray-200" aria-hidden="true" />
+                    <a
+                      className="text-sm font-medium text-gray-700 hover:text-gray-800"
+                      href="#"
+                    >
+                      Create account
+                    </a>
+                  </div>
+
+                  {/* Search */}
+                  <div className="flex lg:ml-6">
+                    <a
+                      className="p-2 text-gray-400 hover:text-gray-500"
+                      href="#"
+                    >
+                      <span className="sr-only">Search</span>
+                      <MagnifyingGlassIcon
+                        className="h-6 w-6"
+                        aria-hidden="true"
+                      />
+                    </a>
+                  </div>
+
+                  {/* Cart */}
+                  <div className="ml-4 flow-root lg:ml-6">
+                    <a className="group -m-2 flex items-center p-2" href="#">
+                      <ShoppingBagIcon
+                        className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
+                        aria-hidden="true"
+                      />
+                      <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">
+                        0
+                      </span>
+                      <span className="sr-only">items in cart, view bag</span>
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </nav>
+        </header>
+        <main>{children}</main>
+      </div>
+    </body>
+  </html>
+);
+
+export default RootLayout;
