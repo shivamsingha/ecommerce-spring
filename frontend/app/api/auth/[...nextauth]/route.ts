34d4df98a5860a27fbbb69747a13bdf3dc5e0910
shivamsingha <shivamgit@pm.me>
2023-08-20 20:52:53 +0530

    Add custom signin page

--- frontend/app/api/auth/[...nextauth]/route.ts
+++ frontend/app/api/auth/[...nextauth]/route.ts
@@ -66,6 +66,9 @@ export const authOptions: AuthOptions = {
     }),
   ],
   debug: process.env.NODE_ENV === "development",
+  pages: {
+    signIn: "/auth/signin",
+  },
   secret: process.env.NEXTAUTH_SECRET,
 };
 
