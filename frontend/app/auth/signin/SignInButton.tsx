7fcbef7023de1c1a53c31ac6db1851cfc297d36e
shivamsingha <shivamgit@pm.me>
2023-08-21 13:32:34 +0530

    Fix redirecting back to signin page after auth

--- frontend/app/auth/signin/SignInButton.tsx
+++ frontend/app/auth/signin/SignInButton.tsx
@@ -10,7 +10,7 @@ export default function SignInButton({ provider }: Props) {
   return (
     <button
       className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-      onClick={() => signIn(provider.id)}
+      onClick={() => signIn(provider.id, { callbackUrl: "/" })}
     >
       Sign in or Sign Up with {provider.name}
     </button>
